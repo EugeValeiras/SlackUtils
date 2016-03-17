b8ed373be50890e0fc9998610320c4ec306791af
Eugenio Jesus Jose Valeiras <Eugenio.valeiras@gmail.com>
2016-03-17 09:48:56 -0300

    init slack utils project

---
+++ src/main/java/utils/slack/SlackField.java
@@ -0,0 +1,31 @@
+package utils.slack;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SlackField {
+
+	@JsonProperty("title")
+	private String title;
+	
+	@JsonProperty("value")
+	private String value;
+
+	public SlackField() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
